c77a9e74c79b5227053a1f30b8e2f8049a1e0113
Rocio <rsoto1@Rocios-MacBook-Pro.local>
2013-09-15 16:19:52 -0400

    More Changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider => 'AWS',
-      :access_key_id => ENV['ACCESS_KEY'],
-      :secret_access_key => ENV['SECRET_KEY']
+      :aws_access_key_id => ENV['ACCESS_KEY'],
+      :aws_secret_access_key => ENV['SECRET_KEY']
     }
  
     config.fog_directory = ENV['AWS_BUCKET']
